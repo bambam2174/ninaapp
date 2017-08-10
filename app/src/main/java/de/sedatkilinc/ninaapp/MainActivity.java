af852f7ea1ed39e51b03993a75dacc9d0874abb1
Sedat Kpunkt <bambam2174@gmail.com>
2017-08-10 08:34:07 +0200

    Initial commit

---
+++ app/src/main/java/de/sedatkilinc/ninaapp/MainActivity.java
@@ -0,0 +1,43 @@
+package de.sedatkilinc.ninaapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.Console;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static String EXTRA_MESSAGE = "de.sedatkilinc.ninaapp.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void editTextBeginEditing(View view) {
+        EditText editText = (EditText)view;
+        editText.setText("");
+    }
+
+    public void sendMessage(View view) {
+        Log.d("Button clicked " , ((Button)view).getText().toString());
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String text = editText.getText().toString();
+        Log.d("Edit Text " , text);
+        intent.putExtra(EXTRA_MESSAGE, text);
+        startActivity(intent);
+    }
+
+    public void openMap(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/de/sedatkilinc/ninaapp/MapsActivity.java
@@ -0,0 +1,88 @@
+package de.sedatkilinc.ninaapp;
+
+import android.Manifest;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private LocationManager mLocationManager;
+    private static long LOCATION_REFRESH_TIME = 5000;
+    private static float LOCATION_REFRESH_DISTANCE = 10;
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d("Location", location.toString());
+            LatLng currLatLong = new LatLng(location.getLatitude(), location.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(currLatLong).title("You are here"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currLatLong));
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        if (canAccessLocation()) {
+            mLocationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, LOCATION_REFRESH_TIME, LOCATION_REFRESH_DISTANCE, mLocationListener);
+        }
+        else {
+            //String[] perms =
+            requestPermissions({Manifest.permission.ACCESS_FINE_LOCATION}, 1340);
+        }
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+    }
+}
